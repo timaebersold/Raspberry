5f827e321c0827f77e0dc64b2b6741f72a22e9d2
Tim Aebersold <tim.aebersold@gmail.com>
2014-07-02 16:08:58 +0200

    LAMPEN Ruby

--- ruby/test.rb
+++ ruby/test.rb
@@ -1,19 +1,118 @@
 require 'wiringpi'
-io = WiringPi::GPIO.new
+@io = WiringPi::GPIO.new
 
-io.write(0,1)
-io.write(4,0)
-io.write(5,0)
-io.write(1,1)
-io.write(0,0)
-io.write(2,1)
-io.write(1,0)
-io.write(3,1)
-io.write(2,0)
-io.write(4,1)
-io.write(5,1)
-io.write(3,0)
-        
+def init()
+  @io.mode(0, OUTPUT)
+  @io.mode(1, OUTPUT)
+  @io.mode(2, OUTPUT)
+  @io.mode(3, OUTPUT)
+  @io.mode(4, OUTPUT)
+  @io.mode(5, OUTPUT)
+end
+
+
+
+
+def bett()
+  sleep 0.5
+end
+
+
+def allean()
+  @io.write(3,1)
+  @io.write(4,1)
+  @io.write(5,1)
+  @io.write(1,1)
+  @io.write(0,1)
+  @io.write(2,1)
+end
+
+def alleaus()
+  @io.write(3,0)
+  @io.write(4,0)
+  @io.write(5,0)
+  @io.write(1,0)
+  @io.write(2,0)
+  @io.write(0,0)
+end
+
+def grünan()
+  
+  @io.write(2,1)
+  @io.write(5,1) 
+end
+
+def grünaus()
+
+  @io.write(2,0)
+  @io.write(5,0)
+
+end
+
+def gelban()
+  
+  @io.write(1,1)
+  @io.write(4,1) 
+end
+
+def gelbaus()
+
+  @io.write(1,0)
+  @io.write(4,0)
+
+end
+
+def rotan()
+  
+  @io.write(0,1)
+  @io.write(3,1) 
+end
+
+def rotaus()
+
+  @io.write(0,0)
+  @io.write(3,0)
+
+end
+
+
+def aufsteigend()
+  @io.write(3,1)
+  bett
+  @io.write(4,1)
+  bett
+  @io.write(5,1)
+  bett
+  @io.write(1,1)
+  bett
+  @io.write(0,1)
+  bett
+  @io.write(2,1)
+  bett
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+init()
+
+
+loop do
+  
+  aufsteigend()
+  sleep 2
+  alleaus
+
+end  
 
 #io.write(pin,value)
 #io.read(pin,value)
